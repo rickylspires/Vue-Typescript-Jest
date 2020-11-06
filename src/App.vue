5b3350c0a399a3171fa31b4f05c7a2d174b8aab2
rickylspires <rickyspires@gmail.com>
2020-11-06 13:57:44 +0000

    First commit - Set up Types, Jest and Navigation

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <section class="section">
+    <div class="container">
+      <Timeline />
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import Timeline from './timeline.vue'
+
+export default defineComponent({
+  components: {
+    Timeline
+  },
+  name: 'App'
+})
+</script>
